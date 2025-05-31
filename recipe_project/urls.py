865144ba915f6fcf676420ccc466f42ee3d784b5
Blairmunk <lemportpro@mail.ru>
2025-05-31 18:13:31 +0300

    fixed display logout page

--- recipe_project/urls.py
+++ recipe_project/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('', include('recipes.urls')),
     path('users/', include('users.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html', next_page='home'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
 
 if settings.DEBUG:
