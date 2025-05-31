075e6f87787f4c80d493adcb57a16c719cb98826
Blairmunk <lemportpro@mail.ru>
2025-05-31 12:26:34 +0300

    Added models to recipes admin

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Recipe, Category, RecipeCategory
 
-# Register your models here.
+class RecipeCategoryInline(admin.TabularInline):
+    model = RecipeCategory
+    extra = 1
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'preparation_time', 'created_at')
+    list_filter = ('author', 'created_at')
+    search_fields = ('title', 'description')
+    prepopulated_fields = {'slug': ('title',)}
+    inlines = [RecipeCategoryInline]
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Category, CategoryAdmin)
